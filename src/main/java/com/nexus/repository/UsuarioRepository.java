768eaa7d305c26e3ebe3f58a55e2de742751f595
juanjo210106 <jgamlop2101@g.educaand.es>
2026-01-09 13:33:04 +0100

    Subida de los repositorios Admin y Usuario

---
+++ src/main/java/com/nexus/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.nexus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nexus.entity.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+
+}
